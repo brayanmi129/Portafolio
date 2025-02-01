bc0f05e72c549c79823ea8ce3c6729663fae74f5
Brayan Miranda <brakcris129@gmail.com>
2025-02-01 13:12:58 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Card from "./components/Card.jsx";
+import Grid from "./components/Grid.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Card />
+
+      <Grid />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Grid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Grid.css";
+
+function Grid() {
+  return (
+    <div id="proyectos" className="grid-container">
+      <div className="grid-item">1</div>
+      <div className="grid-item">2</div>
+      <div className="grid-item">3</div>
+      <div className="grid-item">4</div>
+      <div className="grid-item">5</div>
+      <div className="grid-item">6</div>
+      <div className="grid-item">7</div>
+      <div className="grid-item">8</div>
+    </div>
+  );
+}
+
+export default Grid;
