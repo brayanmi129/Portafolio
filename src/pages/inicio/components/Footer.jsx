e9d0ea27cfd8b81335733ffe97bf16db4b29c043
Brayan Miranda <brakcris129@gmail.com>
2025-09-01 16:51:45 -0500

    refactor footer

--- src/pages/inicio/components/Footer.jsx
+++ src/pages/inicio/components/Footer.jsx
@@ -1,4 +1,3 @@
-import "./Css/Footer.css";
 import React, { useState, useEffect } from "react";
 import GitHubLogo from "../../../assets/github02.svg";
 import LinkedinLogo from "../../../assets/LinkedinLogo.png";
@@ -7,6 +6,7 @@ import wha from "../../../assets/whatsapp.png";
 
 function Footer() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 800);
@@ -14,10 +14,21 @@ function Footer() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   return (
-    <footer id="Contacto" className="footer">
-      <div className="contacto-item">
-        {isMobile ? "" : <p>brayanmiranda129@gmail.com</p>}
+    <footer
+      id="Contacto"
+      className={` flex justify-center items-center w-full text-white 
+        ${isMobile ? "px-2 pb-5 pt-0 min-h-24" : "bg-[#333] p-4"}`}
+      style={{
+        background: isMobile ? "linear-gradient(to bottom, transparent 30%, #333 30%)" : "#333",
+        padding: isMobile ? "0% 2% 5% 2%" : "1rem",
+        minHeight: isMobile ? "24px" : "auto",
+      }}
+    >
+      {/* Gmail */}
+      <div className="w-1/4 flex justify-center items-center">
+        {!isMobile && <p className="mx-4">brayanmiranda129@gmail.com</p>}
         <a
           href="https://mail.google.com/mail/?view=cm&fs=1&to=brayanmiranda129@gmail.com"
           target="_blank"
@@ -25,21 +36,26 @@ function Footer() {
           <img className="w-8" src={GmailLogo} alt="Logo Gmail" />
         </a>
       </div>
-      <div className="contacto-item">
-        {isMobile ? "" : <p> Cel: 3188975553</p>}
+
+      {/* WhatsApp */}
+      <div className="w-1/4 flex justify-center items-center">
+        {!isMobile && <p className="mx-4">Cel: 3188975553</p>}
         <a href="https://api.whatsapp.com/send?phone=573188975553" target="_blank">
           <img className="w-8" src={wha} alt="logo whatsapp" />
         </a>
       </div>
 
-      <div className="contacto-item">
-        {isMobile ? "" : <p>Linkedin: Brayan Miranda</p>}
+      {/* LinkedIn */}
+      <div className="w-1/4 flex justify-center items-center">
+        {!isMobile && <p className="mx-4">Linkedin: Brayan Miranda</p>}
         <a href="https://www.linkedin.com/in/brayan-camilo-miranda-229620253/" target="_blank">
           <img className="w-11" src={LinkedinLogo} alt="Logo linkedin" />
         </a>
       </div>
-      <div className="contacto-item">
-        {isMobile ? "" : <p>Github/brayanmi129</p>}
+
+      {/* GitHub */}
+      <div className="w-1/4 flex justify-center items-center">
+        {!isMobile && <p className="mx-4">Github/brayanmi129</p>}
         <a href="https://github.com/brayanmi129" target="_blank">
           <img className="w-8" src={GitHubLogo} alt="Logo github" />
         </a>
